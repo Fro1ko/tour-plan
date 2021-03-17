66314f288068f8c434916177123a5e796b9f5db0
Vlad Frolov <frolko95@mail.ru>
2021-03-17 22:53:05 +0400

    create: reviews

--- js/main.js
+++ js/main.js
@@ -26,5 +26,23 @@ ymaps.ready(init);
     });
     var myPlacemark = new ymaps.Placemark([51.54, 46.00]);
     myMap.geoObjects.add(myPlacemark);
-}
+};
 
+const swiperReviews = new Swiper('.reviews__swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  autoplay: {
+    delay: 4000
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-button--next',
+    prevEl: '.reviews-button--prev',
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport:true,
+  },
+});
